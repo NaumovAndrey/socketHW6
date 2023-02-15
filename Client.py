409ce5b3e9d2555c2b2b21ee1e7ceb3118110379
naumov <naumov.a.n@icloud.com>
2023-02-15 12:05:05 +0700

    init Server.py

---
+++ Client.py
@@ -0,0 +1,32 @@
+import socket
+import threading
+
+
+def send_message():
+    while True:
+        input_message = 'Andrey: ' + input('> ')
+
+        if input_message == "exit":
+            chat_socket.close()
+            exit(0)
+        chat_socket.send(input_message.encode(encoding='ascii'))
+
+
+def receive_message():
+    while True:
+        received_message = chat_socket.recv(1024)
+        print(received_message)
+
+
+chat_socket = socket.socket()
+address = ('127.0.0.1', 55555)
+chat_socket.connect(address)
+
+name = b"Konstantin"
+chat_socket.send(name)
+
+send_thread = threading.Thread(target=send_message)
+send_thread.start()
+
+recieve_thread = threading.Thread(target=receive_message)
+recieve_thread.start()
