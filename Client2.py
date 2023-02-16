4958ef08f0681d2091600b4b6f3ce59f6d83142f
naumov <naumov.a.n@icloud.com>
2023-02-16 10:23:46 +0700

    переписан сервер

--- Client2.py
+++ Client2.py
@@ -1,32 +1,27 @@
+# После того как соединение произошло, первое сообщение будет ником пользователя, а далее, любое отправленное сообщение будет транслироваться другим клиентам.
+
 import socket
 import threading
 
 
-def send_message():
+def send_mess():
     while True:
-        input_message = 'Alena: ' + input('> ')
-
-        if input_message == "exit":
-            chat_socket.close()
-            exit(0)
-        chat_socket.send(input_message.encode(encoding='ascii'))
+        mes = input("> ")
+        ya_socet.send(mes.encode(encoding='ascii'))
 
 
-def receive_message():
+def get_mess():
     while True:
-        received_message = chat_socket.recv(1024)
-        print(received_message)
+        text = ya_socet.recv(1024)
+        print(text)
 
 
-chat_socket = socket.socket()
-address = ('127.0.0.1', 65432)
-chat_socket.connect(address)
+ya_socet = socket.socket()
+addr = ("127.0.0.1", 65432)
+ya_socet.connect(addr)
 
-name = b"Alena"
-chat_socket.send(name)
+rec_thread = threading.Thread(target=send_mess)
+get_thread = threading.Thread(target=get_mess)
 
-send_thread = threading.Thread(target=send_message)
-send_thread.start()
-
-recieve_thread = threading.Thread(target=receive_message)
-recieve_thread.start()
+rec_thread.start()
+get_thread.start()
--- Server.py
+++ Server.py
@@ -1,16 +1,63 @@
+#!/bin/python3
 import socket
+import threading
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+# Connection Data
+host = '127.0.0.1'
+port = 65432
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+# Starting Server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+# Lists For Clients and Their Nicknames
+clients = []
+nicknames = []
+
+# Sending Messages To All Connected Clients
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+# Handling Messages From Clients
+def handle(client):
+    while True:
+        try:
+            # Broadcasting Messages
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            # Removing And Closing Clients
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast('{} left!'.format(nickname).encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+# Receiving / Listening Function
+def receive():
+    while True:
+        # Accept Connection
+        client, address = server.accept()
+        print("Connected with {}".format(str(address)))
+
+        # Request And Store Nickname
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        # Print And Broadcast Nickname
+        print("Nickname is {}".format(nickname))
+        broadcast("{} joined!".format(nickname).encode('ascii'))
+        client.send('Connected to server!'.encode('ascii'))
+
+        # Start Handling Thread For Client
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+print("Server if listening...")
+receive()
